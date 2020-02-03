53b9d689bdd5643a60227ec8f8498a3ba2b38092
Antonio Marcos Silva de Azambuja <antonioazambuja_@hotmail.com>
2020-02-03 09:31:32 -0300

    ref: add features to medium

--- terraform_aws_ec2_simple/main.tf
+++ terraform_aws_ec2_simple/main.tf
@@ -4,11 +4,31 @@ provider "aws" {
     secret_key = "${var.aws_secret_key}"
 }
 
+resource "aws_security_group" "example" {
+  name = "example"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.ip}/32"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "sg-example"
+  }
+}
+
+
 resource "aws_instance" "example" {
-    key_name      = "${var.key_name}"
-    instance_type = "${var.instance_type}"
-    ami           = "${var.ami_id}"
-    associate_public_ip_address = true
+    ami                    = "${var.ami_id}"
+    key_name               = "${var.key_name}"
+    instance_type          = "${var.instance_type}"
+    vpc_security_group_ids = ["${aws_security_group.example.id}"]
     tags = {
         Name = "example"
     }
--- terraform_aws_ec2_simple/variables.tf
+++ terraform_aws_ec2_simple/variables.tf
@@ -1,12 +1,12 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
-variable "aws_region" {
-  default = "us-east-2"
-}
-
-variable "aws_region_config" {}
+variable "aws_region" {}
 
 variable "instance_type" {}
 
 variable "key_name" {}
+
+variable "ami_id" {}
+
+variable "ip" {}
