3d464443a1ddba5b1e354d2ce34d415b1f08c36e
Antonio Azambuja <antonioazambuja_@hotmail.com>
2020-07-29 21:36:25 -0300

    ref: add comments with use case

--- terraform/aws/providers/providers.tf
+++ terraform/aws/providers/providers.tf
@@ -1,3 +1,4 @@
+# this use case for manage with multiples AWS account and where iam role already exists in your localhost
 provider "aws" {
   alias  = "env"
   region = "${var.REGION}"
@@ -6,6 +7,7 @@ provider "aws" {
   }
 }
 
+# this use case for manage with multiples AWS account and where iam role already exists in your localhost
 provider "aws" {
   alias  = "env2"
   region = "${var.REGION}"
@@ -14,12 +16,14 @@ provider "aws" {
   }
 }
 
+# this use case for manage with single AWS account and where iam role not exists in your localhost
 provider "aws" {
     region     = "${var.AWS_REGION}"
     access_key = "${var.AWS_ACCESS_KEY}"
     secret_key = "${var.AWS_SECRET_KEY}"
 }
 
+# this use case for manage Vault host
 provider "vault" {
   address = base64decode("${var.VAULT_ADDR}")
   token   = base64decode("${var.VAULT_TOKEN}")
